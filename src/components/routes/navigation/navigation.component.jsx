aa027a0f6fbd23e5323df70e79dae87dbc30c1df
CaptainKhan <agaxanist@gmail.com>
2023-04-10 10:36:35 +0400

    files deleted

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,9 +1,13 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from '../../../assets/crown.svg';
 import './navigation.styles.scss';
 
+import { UserContext } from "../../contexts/user.context";
+
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  console.log(currentUser);
   return (
     <Fragment>
       <div className="navigation">
@@ -11,10 +15,10 @@ const Navigation = () => {
           <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
-          <Link to="/shop" className="nav-link">
+          <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link to="/auth" className="nav-link">
+          <Link className="nav-link" to="/auth">
             SIGN IN
           </Link>
         </div>
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import {UserContext} from '../contexts/user.context'
 
 import FormInput from "../../components/form-input/form-input.component";
 import "./sign-in-form.styles.scss";
@@ -8,6 +9,7 @@ import {
   signInAuthUserWithEmailAndPassword
 } from "../../utils/firebase/firebase.utils";
 import Button from "../button/button.component";
+
 
 const defaultFormFields = {
   email: "",
@@ -20,6 +22,11 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
   // console.log(formFields);
+
+  // const val = useContext(UserContext);
+   
+
+  const {setCurrentUser} = useContext(UserContext);
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -34,8 +41,9 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      const response = await signInAuthUserWithEmailAndPassword(email, password);
-      console.log(response);
+      const {user} = await signInAuthUserWithEmailAndPassword(email, password);
+      setCurrentUser(user);
+      
       resetFormFields();
     } catch (error) {
       switch (error.code) {
